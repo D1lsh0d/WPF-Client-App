94c88e63cbcdeae61c5f1d56277f2b2d07966956
Dilshod Khajibaev <dilshod.akk2@gmail.com>
2024-02-24 21:56:25 +0300

    finished users tab

--- LibraryApp/MainWindow.xaml.cs
+++ LibraryApp/MainWindow.xaml.cs
@@ -45,11 +45,6 @@ namespace LibraryApp
             }
         }
         
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            
-        }
-        
 
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -73,7 +68,7 @@ namespace LibraryApp
                 }
             }
         }
-
+        #region "Books tab" 
         private void addBook_Click(object sender, RoutedEventArgs e)
         {
             CreateBook window = new CreateBook();
@@ -134,11 +129,71 @@ namespace LibraryApp
                 MessageBox.Show("Для удаления сначала выберите строку книги, затем нажмите на кнопку \"Удалить\" ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Stop);
             }
         }
+        #endregion
 
+        #region "Users tab"
         private void addUserBtn_Click(object sender, RoutedEventArgs e)
         {
             CreateUser createUser = new CreateUser();
             createUser.ShowDialog();
         }
+
+        private void deleteUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверяем, есть ли выделенные ячейки
+            if (usersDataGrid.SelectedCells.Count > 0)
+            {
+                // Получаем выбранный объект из коллекции
+                var selectedObject = usersDataGrid.SelectedItem;
+
+                if (selectedObject is Users user)
+                {
+                    MessageBoxResult confirmDelete = MessageBox.Show($"Вы уверены что хотите удалить пользователя?", "Подтвердите удаление", MessageBoxButton.YesNo, MessageBoxImage.Hand);
+                    if (confirmDelete == MessageBoxResult.Yes)
+                    {
+                        // Отправка DELETE-запроса на сервер
+                        HttpResponseMessage response = ApiHelper.client.DeleteAsync(ApiHelper.client.BaseAddress + "Users/" + user.Id.ToString()).Result;
+
+                        // Проверка успешности запроса
+                        if (response.IsSuccessStatusCode)
+                        {
+                            MessageBox.Show(response.Content.ReadAsStringAsync().Result, caption: "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                            ApiHelper.usersCollection.Remove(user);
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Error: {response.StatusCode} - {response.ReasonPhrase}", caption: "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Для удаления сначала выберите строку пользователя в таблиуе, затем нажмите на кнопку \"Удалить\" ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+        }
+        
+
+        private void editUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверяем, есть ли выделенные ячейки
+            if (usersDataGrid.SelectedCells.Count > 0)
+            {
+                // Получаем выбранный объект из коллекции
+                var selectedObject = usersDataGrid.SelectedItem;
+
+                if (selectedObject is Users user)
+                {
+                    EditUser editUser = new EditUser(user);
+                    editUser.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Для редактирования сначала выберите строку книги, затем нажмите на кнопку \"Редактировать\" ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+        }
+
+        #endregion
     }
 }
--- LibraryApp/Views/Users/EditUser.xaml.cs
+++ LibraryApp/Views/Users/EditUser.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,43 +15,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace LibraryApp.Windows
+namespace LibraryApp.Views.Users
 {
     /// <summary>
-    /// Логика взаимодействия для EditBook.xaml
+    /// Логика взаимодействия для EditUser.xaml
     /// </summary>
-    public partial class EditBook : Window
+    public partial class EditUser : Window
     {
-        public EditBook(Books book)
+        private Models.Users oldUser;
+        public EditUser(Models.Users user)
         {
             InitializeComponent();
-            DataContext = book;
-        }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
+            DataContext = user; 
+            oldUser = user;
         }
 
         private async void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            Books book = new Books()
+            Models.Users updatedUser = new Models.Users()
             {
-                Id = Int32.Parse(bookId.Text),
-                Author = bookAuthor.Text,
-                Name = bookName.Text,
-                PrintDate = bookPrintDate.SelectedDate,
-                Description = bookDescription.Text
+                Id = oldUser.Id,
+                FullName = FullNameTextBox.Text,
+                DateOfBirth = DateOfBirthPicker.SelectedDate,
+                Address = AddressTextBox.Text,
+                Email = EmailTextBox.Text,
+                Phone = PhoneTextBox.Text
             };
 
             // Преобразование объекта в JSON-строку
-            string jsonContent = Newtonsoft.Json.JsonConvert.SerializeObject(book);
+            string jsonContent = Newtonsoft.Json.JsonConvert.SerializeObject(updatedUser);
 
             // Создание контента запроса
             StringContent content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
             // Отправка PUT-запроса на сервер
-            HttpResponseMessage response = await ApiHelper.client.PutAsync(ApiHelper.client.BaseAddress + "Books/UpdateBook", content);
+            HttpResponseMessage response = await ApiHelper.client.PutAsync(ApiHelper.client.BaseAddress + "Users/", content);
 
             // Проверка успешности запроса
             if (response.IsSuccessStatusCode)
